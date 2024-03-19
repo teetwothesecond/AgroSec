b186c71b222e5360318ac3b41e0c5273a8044273
teetwothesecond <daviddanielthefirst@gmail.com>
2024-03-19 08:05:35 +0100

    changes oo

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ document.getElementById("secIssue").addEventListener('click', ()=>{
     const secForm = document.createElement('form');
     const label = document.createElement('label');
     const input =  document.createElement('input');
+    const lastname = document.createElement('label');
+    const lastnameInput = document.createElement('input');
+    const 
     // 
     container = document.getElementById('Sec-Agri-form-container');
     label.innerText = 'First Name'
     label.setAttribute('for', 'firstname');
     input.setAttribute('type','text');
-    secForm.appendChild(input);
     secForm.appendChild(label);
+    secForm.appendChild(input);
     container.appendChild(secForm);
 })
