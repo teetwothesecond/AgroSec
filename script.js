578cdcf2888f04f717d914d8c5ed3ba1ca625821
teetwothesecond <daviddanielthefirst@gmail.com>
2024-03-20 00:06:30 +0100

    dd
    dd

--- script.js
+++ script.js
@@ -3,16 +3,36 @@ document.getElementById("secIssue").addEventListener('click', ()=>{
     const secForm = document.createElement('form');
     const label = document.createElement('label');
     const input =  document.createElement('input');
-    const lastname = document.createElement('label');
-    const lastnameInput = document.createElement('input');
-    const 
-    // 
+    const severity = document.createElement('label');
+    const severityInput = document.createElement('input');
+    const timestamp = document.createElement('label');
+    const timestampInput = document.createElement('input')
+    const select = document.createElement('select');
+    const textArea = document.createElement('textarea')
+    
+    const security = ['Accident','Theft', 'Car Jacking','Human Trafficking']
+     
     container = document.getElementById('Sec-Agri-form-container');
-    label.innerText = 'First Name'
+    label.innerText = 'Incident Title';
     label.setAttribute('for', 'firstname');
     input.setAttribute('type','text');
+    input.setAttribute('class','form-control')
     secForm.appendChild(label);
     secForm.appendChild(input);
+    security.forEach((item)=>{
+        console.log(item);
+        const option = document.createElement('option');
+        option.value = item;
+        option.innerText = item;
+        select.appendChild(option);
+    });
+    select.style = 'display: block; margin: 5px 0;'
+    severity.innerText = "What is the Severity Level";
+    severityInput.setAttribute('type','number');
+    severityInput.setAttribute('class','form-control')
+    secForm.appendChild(select);
+    secForm.appendChild(textArea);
+
     container.appendChild(secForm);
 })
 
