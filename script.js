767723c2772067e4b68746647073f60ccbac36b4
teetwothesecond <daviddanielthefirst@gmail.com>
2024-03-20 22:52:33 +0100

    Changes to the back Button
    Changes to the back Button

--- script.js
+++ script.js
@@ -29,13 +29,61 @@ document.getElementById("secIssue").addEventListener('click', ()=>{
     select.style = 'display: block; margin: 5px 0;'
     severity.innerText = "What is the Severity Level";
     severityInput.setAttribute('type','number');
-    severityInput.setAttribute('class','form-control')
+    severityInput.setAttribute('class','form-control');
     secForm.appendChild(select);
     secForm.appendChild(textArea);
 
     container.appendChild(secForm);
 })
 
+document.getElementById("Agric-Issues").addEventListener('click', ()=>{
+    const secForm = document.createElement('form');
+    const label = document.createElement('label');
+    const input =  document.createElement('input');
+    const severity = document.createElement('label');
+    const severityInput = document.createElement('input');
+    const timestamp = document.createElement('label');
+    const timestampInput = document.createElement('input')
+    const select = document.createElement('select');
+    const textArea = document.createElement('textarea')
+    
+    const security = ['Accident','Theft', 'Car Jacking','Human Trafficking']
+     
+    container = document.getElementById('Agri-Form-container');
+    label.innerText = 'Incident Title';
+    label.setAttribute('for', 'firstname');
+    input.setAttribute('type','text');
+    input.setAttribute('class','form-control')
+    secForm.appendChild(label);
+    secForm.appendChild(input);
+    security.forEach((item)=>{
+        console.log(item);
+        const option = document.createElement('option');
+        option.value = item;
+        option.innerText = item;
+        select.appendChild(option);
+    });
+    select.style = 'display: block; margin: 5px 0;'
+    severity.innerText = "What is the Severity Level";
+    severityInput.setAttribute('type','number');
+    severityInput.setAttribute('class','form-control');
+    secForm.appendChild(select);
+    secForm.appendChild(textArea);
+
+    container.appendChild(secForm);
+})
+
+document.getElementById('lodgeComplaint').addEventListener('click',()=>{
+    document.getElementById('complainData').style = 'display: inline';
+    document.getElementById('container').style =  'display: none';
+
+})
+
+document.getElementById('backtodashboard').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('complainData').style = 'display: none';
+    document.getElementById('container').style = 'display: inline'
+})
 // Function to trigger the PDF download
 function downloadPDF() {
     // Replace 'path/to/your/pdf.pdf' with the actual path to your PDF file
